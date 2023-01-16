14062a5576d3d47318aa06a75c44225aa7c8ea5f
seungyoonLee <syoon624@naver.com>
2023-01-16 16:44:35 +0900

    feat: 기본 레이아웃 구성

--- src/components/SNB/index.tsx
+++ src/components/SNB/index.tsx
@@ -1,7 +1,77 @@
-import React from "react";
+import styled from 'styled-components';
+import React from 'react';
+import { AiFillHome } from 'react-icons/ai';
 
 const SNB = () => {
-  return <div>SNB</div>;
+	return (
+		<Container>
+			<ul>
+				<section>
+					<li>
+						<i>
+							<AiFillHome color="white" />
+						</i>
+						<p>Home</p>
+					</li>
+					<li>
+						<i>
+							<AiFillHome color="white" />
+						</i>
+						<p>Explore</p>
+					</li>
+					<li>
+						<i>
+							<AiFillHome color="white" />
+						</i>
+						<p>Subscriptions</p>
+					</li>
+				</section>
+				<section>
+					<li>
+						<i>
+							<AiFillHome color="white" />
+						</i>
+						<p>Library</p>
+					</li>
+					<li>
+						<i>
+							<AiFillHome color="white" />
+						</i>
+						<p>History</p>
+					</li>
+					<li>
+						<i>
+							<AiFillHome color="white" />
+						</i>
+						<p>Watch later</p>
+					</li>
+					<li>
+						<i>
+							<AiFillHome color="white" />
+						</i>
+						<p>Linked videos</p>
+					</li>
+				</section>
+				<section>
+					<li>
+						<i>
+							<AiFillHome color="white" />
+						</i>
+						<p>Settings</p>
+					</li>
+					<li>
+						<i>
+							<AiFillHome color="white" />
+						</i>
+						<p>Report history</p>
+					</li>
+				</section>
+			</ul>
+		</Container>
+	);
 };
 
+const Container = styled.nav`
+	background-color: black;
+`;
 export default SNB;
