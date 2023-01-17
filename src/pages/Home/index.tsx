1c4bfd4f43df539d3b96989076fcefa03f5a289a
Hyeji Kong <109419531+hayden365@users.noreply.github.com>
2023-01-17 15:54:05 +0900

    feat: video화면 추가

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
+<<<<<<< HEAD
 import VideoContainer from '../../components/VideoContainer/VideoContainer';
+=======
+>>>>>>> 76a90be (feat: video화면 추가)
 
 const Home = () => {
 	return (
