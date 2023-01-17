76a90be1c7e764e624a20a274aa23b0f9bb301e1
Hyeji Kong <109419531+hayden365@users.noreply.github.com>
2023-01-17 15:54:05 +0900

    feat: video화면 추가

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -1,11 +1,10 @@
 import axios from 'axios';
-
-const { VITE_API_KEY } = import.meta.env;
+let { REACT_APP_YOUTUBE_API_KEY } = process.env;
 
 const instance = axios.create({
 	baseURL: 'https://youtube.googleapis.com/youtube/v3',
 	params: {
-		key: VITE_API_KEY,
+		key: `${REACT_APP_YOUTUBE_API_KEY}`,
 	},
 });
 
---
+++ src/components/VideoContainer/VideoBox.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const VideoBox = () => {
+	return <div>VideoBox</div>;
+};
+
+export default VideoBox;
