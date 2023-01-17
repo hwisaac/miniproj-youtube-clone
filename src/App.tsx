93330919a6ad34bdfa3ebd9f364f8868467cf224
Hyeji Kong <109419531+hayden365@users.noreply.github.com>
2023-01-17 16:10:41 +0900

    doc

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import SNB from './components/SNB';
 import VideoDisplay from './components/VideoContainer';
+import { Reset } from 'styled-reset';
 
 function App() {
 	const [toggle, setToggle] = useState(false);
