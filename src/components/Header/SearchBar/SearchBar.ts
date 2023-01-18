431cdd6f915e7d9ecd05a64790b49c72a8c675e3
Hyojina <hjkim3009@gmail.com>
2023-01-18 13:26:25 +0900

    feat: pull하기 위한 커밋

--- src/components/Header/SearchBar/SearchBar.ts
+++ src/components/Header/SearchBar/SearchBar.ts
@@ -1,18 +1,54 @@
 import styled from 'styled-components';
 
-const Container = styled.div`
+const Icon = styled.i`
 	display: flex;
 	align-items: center;
+	padding: 10px;
+
+	svg {
+		color: white;
+		width: 22px;
+		height: 22px;
+	}
+
+	&:hover {
+		background-color: #242424;
+		border-radius: 20px;
+	}
+`;
+
+const SearchIcon = styled.i<{ focus: boolean }>`
+	display: ${(props) => (props.focus ? 'flex' : 'none')};
+	align-items: center;
+	padding: 10px;
+	position: absolute;
+	left: 70px;
+
+	svg {
+		color: whitesmoke;
+		width: 20px;
+		height: 20px;
+	}
+`;
+
+const Container = styled.div`
+	max-width: 700px;
+	width: 100vw;
 
 	form {
+		width: 100%;
 		display: flex;
+		justify-content: right;
 		align-items: center;
 		position: relative;
 	}
 
 	.search-box {
 		background-color: #080808;
-		width: 540px;
+		width: 70%;
+		box-sizing: border-box;
+		max-width: 540px;
+		min-width: 48px;
 		font-size: 16px;
 		color: white;
 		border: 1px solid #343a40;
@@ -20,20 +56,21 @@ const Container = styled.div`
 		border-bottom-left-radius: 20px;
 		border-right: 0px;
 		padding-left: 16px;
-		height: 36px;
+		height: 40px;
 	}
 	.search-box:focus {
 		outline: none;
 		border: 1px solid royalblue;
-		width: 539px;
+		width: calc(70% + 30px);
+		padding-left: 46px;
 	}
 
 	.search-button {
 		display: flex;
 		align-items: center;
+		justify-content: center;
 		background-color: #242424;
 		border: 1px solid #303030;
-		border-left: 0px;
 		border-top-right-radius: 20px;
 		border-bottom-right-radius: 20px;
 		width: 64px;
@@ -42,17 +79,24 @@ const Container = styled.div`
 
 	.keyboard {
 		position: absolute;
-		right: 64px;
+		right: 116px;
+		padding: 7px;
+		border-top-left-radius: 18px;
+		border-bottom-left-radius: 18px;
+		background-color: #080808;
 		svg {
+			background-color: #080808;
 			color: gray;
 		}
 		&:hover {
-			background-color: transparent;
-			border-radius: 20px;
 			svg {
 				color: whitesmoke;
+				border-radius: 0;
 			}
 		}
+		/* @media screen and (min-width: 1350px) {
+			right: 50px;
+		} */
 	}
 	.search {
 		&:hover {
@@ -65,24 +109,10 @@ const Container = styled.div`
 		background-color: #121212;
 		margin-left: 10px;
 	}
-`;
-
-const Icon = styled.i`
-	display: flex;
-	align-items: center;
-	padding: 10px;
-	color: white;
-
-	svg {
-		color: white;
-		width: 20px;
-		height: 20px;
-	}
-
-	&:hover {
+	.voice:hover {
 		background-color: #242424;
 		border-radius: 20px;
 	}
 `;
 
-export { Container, Icon };
+export { Container, Icon, SearchIcon };
